5e57d4bcec1aea01c1aaf78ac22eb11a8b297abf
Chandrasekhar Tammi <Chandrasekhar.Tammi@perfectchannel.com>
2018-01-17 11:02:35 +0000

    Add project files.

---
+++ ShoppingCart/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace ShoppingCart
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "Products",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+          //  var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().FirstOrDefault();
+          //  jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ ShoppingCart/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShoppingCart.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Apples", Description = "Fruit",Stock =5, Price = 2.5M },
+            new Product { Id = 2, Name = "Bread", Description = "Loaf",Stock =10,  Price = 1.35M },
+            new Product { Id = 3, Name = "Oranges", Description = "Fruit",Stock =10,  Price = 2.99M },
+            new Product { Id = 4, Name = "Milk", Description = "Milk",Stock =3,  Price = 2.07M },
+            new Product { Id = 5, Name = "Chocolate", Description = "Bars",Stock =20,  Price = 11.79M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
